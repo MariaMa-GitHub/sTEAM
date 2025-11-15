3d16b72e5a86e981a650f3e03d748e0ba1de4f35
Iris Li <irisss1717@gmail.com>
2025-11-14 23:49:27 -0500

    tooltip does not work

--- js/game-lobby.js
+++ js/game-lobby.js
@@ -1,12 +1,15 @@
 
 // SVG drawing area
 
-let margin = {top: 40, right: 10, bottom: 60, left: 60};
+let margin = {top: 20, right: 20, bottom: 20, left: 20};
 
-let width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+let width = 1200 - margin.left - margin.right;
+let height = 700 - margin.top - margin.bottom;
+
+const icons = ['./red-person.png', './blue-person.png'];
 
 let svg = d3.select("#game-lobby").append("svg")
+    .attr("class", "main-lobby")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   	.append("g")
@@ -54,12 +57,14 @@ indieLegend.append("text")
     .attr("text-anchor", "middle")
     .attr("fill", "#c7d5e0")
     .attr("font-family", "ui-monospace, 'Courier New', monospace")
-    .attr("font-size", "12px");
+    .attr("font-size", "12px")
+    .attr("y", 10);
 
-indieLegend.append("circle")
-    .attr("r", 5)
-    .attr("fill", "red")
-    .attr("cx", 75);
+indieLegend.append("image")
+    .attr("width", 16)
+    .attr("height", 16)
+    .attr('xlink:href', icons[0])
+    .attr("x", 75);
 
 let nonIndieLegend = svg.append("g")
     .attr("transform", "translate(50, " + (height - 25) + ")");
@@ -69,12 +74,18 @@ nonIndieLegend.append("text")
     .attr("text-anchor", "middle")
     .attr("fill", "#c7d5e0")
     .attr("font-family", "ui-monospace, 'Courier New', monospace")
-    .attr("font-size", "12px");
+    .attr("font-size", "12px")
+    .attr("y", 10);
 
-nonIndieLegend.append("circle")
-    .attr("r", 5)
-    .attr("fill", "blue")
-    .attr("cx", 75);
+nonIndieLegend.append("image")
+    .attr("width", 16)
+    .attr("height", 16)
+    .attr('xlink:href', icons[1])
+    .attr("x", 75);
+
+// append tooltip
+tooltip = d3.select(".main-lobby").append('div')
+    .attr('class', "tooltip")
 
 // Create a 'data' property under the window object
 // to store the data
@@ -112,45 +123,47 @@ function changeSelection(cooperativeData, competitiveData) {
         .duration(500)
         .text("Competitive")
 
-    leftPlayer = leftTeam.selectAll("circle")
+    leftPlayer = leftTeam.selectAll("image")
         .data(cooperativeData);
 
     // Enter
-    leftPlayer.enter().append("circle")
+    leftPlayer.enter().append("image")
         .attr("class", "left-player")
 
     // Enter and Update
         .merge(leftPlayer)
-        .attr("cx", (d, index) => (index % 30) * width / 60 - 15)
-        .attr("cy", (d, index) => Math.floor(index / 30) * height / 30 + margin.top)
-        .attr("r", 5)
-        .attr("fill", d => {
+        .attr("x", (d, index) => (index % 30) * width / 60 - 15)
+        .attr("y", (d, index) => Math.floor(index / 30) * height / 30 + margin.top)
+        .attr("width", 16)
+        .attr("height", 16)
+        .attr("xlink:href", d => {
             if (d.genres.includes('Indie'))
-                return "red"
+                return icons[0]
             else
-                return "blue"
+                return icons[1]
         });
 
     // Exit
     leftPlayer.exit().remove();
 
-    let rightPlayer = rightTeam.selectAll("circle")
+    let rightPlayer = rightTeam.selectAll("image")
         .data(competitiveData);
 
     // Enter
-    rightPlayer.enter().append("circle")
+    rightPlayer.enter().append("image")
         .attr("class", "right-player")
 
     // Enter and Update
         .merge(rightPlayer)
-        .attr("cx", (d, index) => (index % 30) * width / 60 + width / 2 + 15)
-        .attr("cy", (d, index) => Math.floor(index / 30) * height / 30 + margin.top)
-        .attr("r", 5)
-        .attr("fill", d => {
+        .attr("x", (d, index) => (index % 30) * width / 60 + width / 2 + 15)
+        .attr("y", (d, index) => Math.floor(index / 30) * height / 30 + margin.top)
+        .attr("width", 16)
+        .attr("height", 16)
+        .attr("xlink:href", d => {
             if (d.genres.includes('Indie'))
-                return "red"
+                return icons[0]
             else
-                return "blue"
+                return icons[1]
         });
 
     // Exit
@@ -190,45 +203,94 @@ function updateVisualization() {
     let cooperativeData = multiplayerData.filter(d => d.categories.includes('Co-op'));
     let competitiveData = multiplayerData.filter(d => d.categories.includes('PvP'));
 
-    let leftPlayer = leftTeam.selectAll("circle")
+    console.log(multiplayerData)
+
+    let numIndieMultiplayer;
+    let numNonindieMultiplayer;
+    let numIndieNonmultiplayer;
+    let numNonindieNonmultiplayer;
+    let numIndieCooperative;
+    let numNonindieCooperative;
+    let numIndieCompetitive;
+    let numNonindieCompetitive;
+
+    for (let i = 0; i < multiplayerData; i++) {
+        if (multiplayerData[i].genres.includes('Indie')) {
+            numIndieMultiplayer++;
+        }
+        else {
+            numNonindieMultiplayer++;
+        }
+    }
+
+    for (let i = 0; i < nonmultiplayerData; i++) {
+        if (nonmultiplayerData[i].genres.includes('Indie')) {
+            numIndieNonmultiplayer++;
+        }
+        else {
+            numNonindieNonmultiplayer++;
+        }
+    }
+
+    for (let i = 0; i < cooperativeData; i++) {
+        if (cooperativeData[i].genres.includes('Indie')) {
+            numIndieCooperative++;
+        }
+        else {
+            numNonindieCooperative++;
+        }
+    }
+
+    for (let i = 0; i < competitiveData; i++) {
+        if (competitiveData[i].genres.includes('Indie')) {
+            numIndieCompetitive++;
+        }
+        else {
+            numNonindieCompetitive++;
+        }
+    }
+
+    let leftPlayer = leftTeam.selectAll("image")
 		.data(multiplayerData);
 
 	// Enter
-	leftPlayer.enter().append("circle")
+	leftPlayer.enter().append("image")
 		.attr("class", "left-player")
 
 	// Enter and Update
 		.merge(leftPlayer)
-		.attr("cx", (d, index) => (index % 30) * width / 60 - 15)
-		.attr("cy", (d, index) => Math.floor(index / 30) * height / 30 + margin.top)
-		.attr("r", 5)
-		.attr("fill", d => {
+		.attr("x", (d, index) => (index % 30) * width / 60 - 15)
+		.attr("y", (d, index) => Math.floor(index / 30) * height / 30 + margin.top)
+		.attr("width", 16)
+        .attr("height", 16)
+		.attr("xlink:href", d => {
             if (d.genres.includes('Indie'))
-                return "red"
+                return icons[0]
             else
-                return "blue"
+                return icons[1]
         });
 
 	// Exit
 	leftPlayer.exit().remove();
 
-    let rightPlayer = rightTeam.selectAll("circle")
+    let rightPlayer = rightTeam.selectAll("image")
 		.data(nonmultiplayerData);
 
 	// Enter
-	rightPlayer.enter().append("circle")
+	rightPlayer.enter().append("image")
 		.attr("class", "right-player")
 
 	// Enter and Update
 		.merge(rightPlayer)
-		.attr("cx", (d, index) => (index % 30) * width / 60 + width / 2 + 15)
-		.attr("cy", (d, index) => Math.floor(index / 30) * height / 30 + margin.top)
-		.attr("r", 5)
-		.attr("fill", d => {
+		.attr("x", (d, index) => (index % 30) * width / 60 + width / 2 + 15)
+		.attr("y", (d, index) => Math.floor(index / 30) * height / 30 + margin.top)
+		.attr("width", 16)
+        .attr("height", 16)
+		.attr("xlink:href", d => {
             if (d.genres.includes('Indie'))
-                return "red"
+                return icons[0]
             else
-                return "blue"
+                return icons[1]
         });
 
 	// Exit
@@ -238,17 +300,35 @@ function updateVisualization() {
         .attr("width", width / 2)
         .attr("height", height)
         .style("opacity", 0)
-        .on("mouseover", function (d) {            
+        .on("mousemove", function (event, d) {            
             d3.selectAll(".left-player")
                 .transition()
                 .duration(200)
-                .attr("r", 7);
+                .attr("width", 20)
+                .attr("height", 20);
+
+            tooltip
+                .style("opacity", 1)
+                .style("left", event.pageX + 20 + "px")
+                .style("top", event.pageY + "px")
+                .html(`
+                    <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 20px">
+                        <h4> Number of indie games: ${numIndieMultiplayer}</h4>      
+                        <h4> Number of non-indie games: ${numNonindieMultiplayer}</h4>                      
+                    </div>`);
         })
         .on("mouseleave", function () {
             d3.selectAll(".left-player")
                 .transition()
                 .duration(200)
-                .attr("r", 5 );
+                .attr("width", 16)
+                .attr("height", 16);
+
+            tooltip
+                .style("opacity", 0)
+                .style("left", 0)
+                .style("top", 0)
+                .html(``);
         })
         .on("click", function (d) {
             changeSelection(cooperativeData, competitiveData)
@@ -259,16 +339,34 @@ function updateVisualization() {
         .attr("width", width / 2)
         .attr("height", height)
         .style("opacity", 0)
-        .on("mouseover", function (d) {            
+        .on("mousemove", function (event, d) {            
             d3.selectAll(".right-player")
                 .transition()
                 .duration(200)
-                .attr("r", 7);
+                .attr("width", 20)
+                .attr("height", 20);
+
+            tooltip
+                .style("opacity", 1)
+                .style("left", event.pageX + 20 + "px")
+                .style("top", event.pageY + "px")
+                .html(`
+                    <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 20px">
+                        <h4> Number of indie games: ${numIndieNonmultiplayer}</h4>      
+                        <h4> Number of non-indie games: ${numNonindieNonmultiplayer}</h4>                      
+                    </div>`);
         })
         .on("mouseleave", function () {
             d3.selectAll(".right-player")
                 .transition()
                 .duration(200)
-                .attr("r", 5 );
+                .attr("width", 16)
+                .attr("height", 16);
+            
+            tooltip
+                .style("opacity", 0)
+                .style("left", 0)
+                .style("top", 0)
+                .html(``);
         });
 }
